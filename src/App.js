535fa1d2f58fd5a3ab218295dfd4ecf90b9f9ac8
Narges <bish_bahar@yahoo.com>
2023-05-07 00:58:19 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by 
-        <a href="https://github.com/bishbahar"> Narges Bishbahar </a> and is
-        <a
-          href="https://github.com/bishbahar/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        > open-sourced </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by
+          <a href="https://github.com/bishbahar"> Narges Bishbahar </a> and is
+          <a
+            href="https://github.com/bishbahar/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
