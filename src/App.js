4183ce6e2d0b7b496879dd8489a0733b5c620092
Narges <bish_bahar@yahoo.com>
2023-05-06 21:27:01 +0330

    added the github link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+      Weather App
+      <footer>
+        This project was coded by 
+        <a href="https://github.com/bishbahar"> Narges Bishbahar </a> and is
+        <a
+          href="https://github.com/bishbahar/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        > open-sourced </a>
+      </footer>
     </div>
   );
 }
