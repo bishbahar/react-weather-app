c6366e82d85f1d30d01696b54792bcfd1ff3aa78
Narges <bish_bahar@yahoo.com>
2023-05-07 01:17:50 +0330

    added html codes

---
+++ src/weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./weather.css";
+
+export default function Weather() {
+  return (
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Dezful</h1>
+      <ul>
+        <li>Sunday 00:00</li>
+        <li>Clear</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII="
+            alt="Clear"
+          />
+          28°C|F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 25%</li>
+            <li>Wind: 3 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
