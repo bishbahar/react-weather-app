1ec512960fcf7a7364986ec0d4f8007b59e31f07
Narges <bish_bahar@yahoo.com>
2023-05-07 11:59:00 +0330

    added API

--- src/weather.js
+++ src/weather.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios";
 import "./weather.css";
 
 export default function Weather() {
-  return (
+  const [weatherData,setWeatherData]=useState({ready:false})
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready:true,
+      temprature:response.data.main.temp,
+      humidity:response.data.main.humidity,
+      description:response.data.weather[0].description,
+      date:"Sunday 00:00",
+      iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      wind:response.data.wind.speed,
+      city:response.data.name
+    })
+  }
+  
+  if(weatherData.ready){
+return (
     <div className="weather">
       <form>
         <div className="row">
@@ -19,28 +36,36 @@ export default function Weather() {
           </div>
         </div>
       </form>
-      <h1>Dezful</h1>
+      <h1>{weatherData.city}</h1>
       <ul>
-        <li>Sunday 00:00</li>
-        <li>Clear</li>
+        <li>{weatherData.date}</li>
+        <li>{weatherData.description}</li>
       </ul>
       <div className="row">
         <div className="col-6">
           <img
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII="
-            alt="Clear"
+            src={weatherData.iconUrl}
+            alt={weatherData.description}
           />
-          <span className="temperature">28</span>
+          <span className="temperature">{Math.round(weatherData.temprature)}</span>
           <span className="unit">°C | F</span>
         </div>
         <div className="col-6">
           <ul>
             <li>Precipitation: 0%</li>
-            <li>Humidity: 25%</li>
-            <li>Wind: 3 km/h</li>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {Math.round(weatherData.wind)} km/h</li>
           </ul>
         </div>
       </div>
     </div>
   );
+  }else{
+const apiKey = "a62bf47452d78ba41c221fd3cb539691";
+let city = "London";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse);
+
+return "Loding..."
+  }
 }
